61e4281e82e8813a087667fdf705bcc188203ac0
Yuri Matos Rodrigues <yurimatos.tuti@gmail.com>
2020-05-05 22:24:58 -0300

    Criação da estrutura do projeto EvenTons

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace evenTons.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
